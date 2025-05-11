d538f655e7e26e2c1aa523bf793611ea59fd8de7
mhdrefdi <refdipsb23@gmail.com>
2025-05-12 05:31:32 +0700

    update journals to public api

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,10 @@ Route::post('me', \App\Http\Controllers\MeController::class)->middleware('auth:a
 Route::prefix('ojs')->group(function () {
     Route::post('register', [\App\Http\Controllers\OJS\AuthController::class, 'register']);
 
+    Route::get('journals', [\App\Http\Controllers\OJS\JournalController::class, 'index']);
+    Route::get('journals/{journal}', [\App\Http\Controllers\OJS\JournalController::class, 'show']);
+
     Route::middleware('auth:api')->group(function () {
-
-        Route::get('journals', [\App\Http\Controllers\OJS\JournalController::class, 'index']);
-        Route::get('journals/{journal}', [\App\Http\Controllers\OJS\JournalController::class, 'show']);
-
         Route::get('submissions', [\App\Http\Controllers\OJS\SubmissionController::class, 'index']);
         Route::get('submissions/{submission}', [\App\Http\Controllers\OJS\SubmissionController::class, 'show']);
         Route::post('submissions/{journal}', [\App\Http\Controllers\OJS\SubmissionController::class, 'store']);
